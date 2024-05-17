3fd21bb59e03ba8b0541e5b378171f5568ae6348
emma <hi@emma.cafe>
2024-05-17 00:11:07 -0400

    initial commit

---
+++ public/arbitrarySWPath/sw.js
@@ -0,0 +1,1 @@
+importScripts("https://cdn.jsdelivr.net/npm/replaywebpage@2.0.0/sw.js");
---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  const replayWebURL = "https://cdn.jsdelivr.net/npm/replaywebpage@2.0.0/ui.js";
+
+  const isScriptAlreadyIncluded = (src) => {
+    const scripts = document.getElementsByTagName("script");
+    for (let i = 0; i < scripts.length; i++)
+      if (scripts[i].getAttribute("src") === src) return true;
+    return false;
+  };
+
+  const loadScript = async (url) =>
+    new Promise((resolve) => {
+      if (!isScriptAlreadyIncluded(url)) {
+        const script = document.createElement("script");
+        script.onload = () => {
+          resolve();
+        };
+        script.src = url;
+        document.head.appendChild(script);
+      } else {
+        resolve();
+      }
+    });
+
+  (async () => {
+    try {
+      await loadScript(replayWebURL);
+    } catch (e) {
+      console.error(e);
+    }
+  })();
+</script>
+
+<h1>Svelte RWP Example</h1>
+<replay-web-page
+  style="height: 600px;"
+  source="https://replayweb.page/docs/examples/tweet-example.wacz"
+  replayBase="arbitrarySWPath/"
+></replay-web-page>
